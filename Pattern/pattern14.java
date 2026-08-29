277a736bbd52a9eab270faf10fc7fbe907a120e5
Ananya <ananyax2805@gmail.com>
2026-08-30 00:56:24 +0530

    Add pattern programs 14 and 15

---
+++ Pattern/pattern14.java
@@ -0,0 +1,15 @@
+public class pattern14 {
+    public static void main(String[] args) {
+        int c=0;
+        for(int i=0;i<5;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                c++;
+                System.out.print(" "+ c);
+            }
+            System.out.println();
+        }
+    }
+    
+}
