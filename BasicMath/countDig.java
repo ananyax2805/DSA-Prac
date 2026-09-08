e0b46694f973480c2d0633c9a1a4060a7f651c36
Ananya <ananyax2805@gmail.com>
2026-09-08 23:02:21 +0530

    Add count dig

---
+++ BasicMath/countDig.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class countDig {
+    public static void main(String[] args)
+    {
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
+      int count=0;
+      while(n!=0)
+      {
+          int k=n%10;
+          count++;
+          n=n/10;
+      }
+      System.out.println(count);
+       
+        
+    }
+}
